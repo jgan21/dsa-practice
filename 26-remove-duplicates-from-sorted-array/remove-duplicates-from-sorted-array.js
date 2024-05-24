a0dca4f9cff0b279ceb846b9916548afe9a34727
Jia Rong Gan <107450997+jgan21@users.noreply.github.com>
2024-05-24 11:55:38 -0500

    Time: 66 ms (51.48%) | Memory: 52.2 MB (51.48%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,25 +3,25 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-//     let [left, right] = [0, 1];
-//     let hold = 1;
+    let [left, right] = [0, 1];
+    let hold = 1;
     
-//     while (right < nums.length){
-//         if (nums[left] !== nums[right]){
-//             nums[hold] = nums[right];
-//             hold += 1; 
-//             left = right;
-//         }
-//         right += 1;
-//     }
-//     return hold;
-// };
-    let i = 0;
-    for (let j = 1; j < nums.length; j++){
-        if (nums[j] !== nums[i]){
-            i++;
-            nums[i] = nums[j]
+    while (right < nums.length){
+        if (nums[left] !== nums[right]){
+            nums[hold] = nums[right];
+            hold += 1; 
+            left = right;
         }
+        right += 1;
     }
-    return i + 1
-}
+    return hold;
+};
+//     let i = 0;
+//     for (let j = 1; j < nums.length; j++){
+//         if (nums[j] !== nums[i]){
+//             i++;
+//             nums[i] = nums[j]
+//         }
+//     }
+//     return i + 1
+// }
