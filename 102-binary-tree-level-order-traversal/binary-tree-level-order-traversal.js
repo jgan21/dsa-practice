20f8145d8140c7392c85d0ea45457fe988aa9f90
Jia Rong Gan <107450997+jgan21@users.noreply.github.com>
2025-04-24 19:04:12 -0700

    Time: 3 ms (47.86%) | Memory: 58.1 MB (70.67%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -11,21 +11,35 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    if (!root) return [];
-    let result = [];
-    const queue = [root];
+//     if (!root) return [];
+//     let result = [];
+//     const queue = [root];
 
-    while (queue.length > 0){
-        let levelSize = queue.length;
-        const currentLevel = [];
+//     while (queue.length > 0){
+//         let levelSize = queue.length;
+//         const currentLevel = [];
 
-        for (let i = 0; i < levelSize; i++){
-            const node = queue.shift();
-            currentLevel.push(node.val)
-            if (node.left)queue.push(node.left);
-            if (node.right)queue.push(node.right);
+//         for (let i = 0; i < levelSize; i++){
+//             const node = queue.shift();
+//             currentLevel.push(node.val)
+//             if (node.left)queue.push(node.left);
+//             if (node.right)queue.push(node.right);
+//         }
+//         result.push(currentLevel);
+//     }
+//     return result;
+// };
+    const result = [];
+    function dfs(node, depth){
+        if (!node) return;
+        if (result.length === depth){
+            result.push([])
         }
-        result.push(currentLevel);
+        result[depth].push(node.val)
+
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
     }
+    dfs(root, 0)
     return result;
 };
